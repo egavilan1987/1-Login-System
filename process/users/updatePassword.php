a86ddd677f06a4ac81459149d307466870f4effa
egavilan1987 <enmanuelgc@gmail.com>
2018-10-23 11:40:53 -0400

    Update updatePassword.php

--- process/users/updatePassword.php
+++ process/users/updatePassword.php
@@ -1,16 +1,13 @@
 <?php 
 	session_start();
-	require_once "../../model/connection.php";
-	require_once "../../model/Users.php";
+	require_once "../../classes/connection.php";
+	require_once "../../classes/users.php";
 	$obj= new users;
 
 	$updatePassword=sha1($_POST['password']);
 
 
 	$data=array(
-				$_POST['idUpdatePassword'],
-			    $updatePassword,
-			    $_SESSION['user']
 				);  
 	echo $obj->updatePassword($data);
  ?>
